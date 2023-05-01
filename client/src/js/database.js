e1f84c8905b10c3db209dea85a6a86a978736696
Raeludwig <racheleludwig@gmail.com>
2023-05-01 13:15:23 -0600

    updated logiv in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,28 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// TO DONE: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+const db = await openDB('jate', 1);
+const tx = db.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const result= await store.put(content);
+console.log(`     -date saved to the database`,result);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+};
+
+// TO DONE: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb not implemented');
+const db = await openDB('jate', 1);
+const tx = db.transaction('jate', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+const result= await request;
+console.log(`result.value`,result);
+return result;
+}
 
 initdb();
